8bb6be5c8e356ae931383c3f197ec7837b183204
Ruan Gabriel <101756709+Ruann1@users.noreply.github.com>
2024-09-14 00:24:53 -0300

    adição barra lateral

--- projeto-mapas/javascript.js
+++ projeto-mapas/javascript.js
@@ -1,8 +1,17 @@
-let barra = document.getElementById('barralateral')
-
 function teste(){
 
-document.getElementById('barralateral').style.visibility = 'visible'
+    document.getElementById('barralateral').style.width = '600px'
+
+    document.getElementById('main').style.marginRight = '500px'
+
+}
+
+function fecharbarra(){
+
+    document.getElementById('barralateral').style.width = '0px'
+
+    document.getElementById('main').style.marginRight = '0px'
+
 
 }
 

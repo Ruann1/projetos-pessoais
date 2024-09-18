b7b9e0c1dd50de62d9ba324a54940d0382cf2d36
Ruan Gabriel <101756709+Ruann1@users.noreply.github.com>
2024-09-17 21:55:49 -0300

    update javascript

--- projeto-mapas/javascript.js
+++ projeto-mapas/javascript.js
@@ -1,8 +1,24 @@
-function teste(){
+function mirage(){
 
     document.getElementById('barralateral').style.width = '600px'
 
     document.getElementById('main').style.marginRight = '500px'
+
+    
+
+    document.getElementById('conteudobarra').innerHTML = '<img src="mapas/mirage.jpg"> <h1>Mirage</h1> <p>A Mirage é um dos mapas mais icônicos de Counter-Strike: Global Offensive (CS), com uma ambientação de estilo desértico, inspirado em cidades do Oriente Médio ou Norte da África. É um mapa equilibrado e amplamente utilizado em partidas competitivas, tanto por Terroristas (T) quanto por Contra-Terroristas (CT).<p>O mapa tem dois bombsites, A e B, cada um com características distintas: Bombsite A: Localizado em uma área mais aberta, é acessado por rampas, uma varanda (Palácio) e conectores. Oferece boas oportunidades para uso de granadas de fumaça e controle com snipers (AWP). Bombsite B: É mais fechado, acessado principalmente por um corredor elevado (apps) e uma cozinha (kitchen), exigindo estratégias rápidas e precisas, com controle de utilitários sendo essencial.</p> <p>O controle do meio do mapa é um ponto-chave para a estratégia, pois dá acesso a rotas para os dois bombsites e favorece a mobilidade entre eles.Por sua complexidade e equilíbrio, o Mirage é um mapa de escolha frequente em campeonatos profissionais e é conhecido por permitir uma variedade de táticas, tanto ofensivas quanto defensivas.</p></p>'
+
+}
+
+function ancient(){
+
+    document.getElementById('barralateral').style.width = '600px'
+
+    document.getElementById('main').style.marginRight = '500px'
+
+    
+
+    document.getElementById('conteudobarra').innerHTML = '<img src="mapas/ancient.jpg"> <h1>Ancient</h1> <p><p><strong>Ancient</strong> é um mapa de <strong>Counter-Strike: Global Offensive (CS:GO)</strong> que se passa em um cenário de ruínas antigas, inspirado por civilizações mesoamericanas, com vegetação densa e estruturas de pedra. Lançado em 2021, <strong>Ancient</strong> rapidamente se tornou um dos mapas favoritos por sua estética única e design desafiador, que valoriza o trabalho em equipe e o controle de áreas-chave.</p> <p>Aqui estão os principais pontos:</p> <p><strong>Bombsite A</strong>: Localizado em uma área mais ampla, cercado por paredes de pedra e com múltiplos pontos de entrada, como "main" e "caverna". A comunicação entre os jogadores e o uso de granadas são fundamentais para dominar este ponto.</p> <p><strong>Bombsite B</strong>: Um pouco mais fechado, com entradas por meio de "rampa" e "caverna". É comum ver confrontos rápidos nessa área, exigindo precisão nas execuções e controle da movimentação dos jogadores.</p> <p><strong>Meio</strong>: Como em muitos outros mapas, o controle do meio é crucial. Em Ancient, ele proporciona acesso rápido a ambos os bombsites e permite flanquear o inimigo de forma mais eficiente. O domínio desta área pode decidir o rumo da partida.</p> <p>O mapa é conhecido por suas passagens estreitas, que favorecem combates táticos e o uso inteligente de utilitários (smokes, flashes e molotovs). Ele oferece um grande desafio estratégico para jogadores, com muitos cantos e corredores estreitos, além de uma forte ênfase no domínio de áreas-chave.</p></p>'
 
 }
 

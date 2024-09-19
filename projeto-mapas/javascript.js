8850c2abdeb35ff44c599f9409ac53940edbfaf9
Ruan Gabriel <101756709+Ruann1@users.noreply.github.com>
2024-09-19 11:55:15 -0300

    finalização dos artigos

--- projeto-mapas/javascript.js
+++ projeto-mapas/javascript.js
@@ -18,7 +18,67 @@ function ancient(){
 
     
 
-    document.getElementById('conteudobarra').innerHTML = '<img src="mapas/ancient.jpg"> <h1>Ancient</h1> <p><p><strong>Ancient</strong> é um mapa de <strong>Counter-Strike: Global Offensive (CS:GO)</strong> que se passa em um cenário de ruínas antigas, inspirado por civilizações mesoamericanas, com vegetação densa e estruturas de pedra. Lançado em 2021, <strong>Ancient</strong> rapidamente se tornou um dos mapas favoritos por sua estética única e design desafiador, que valoriza o trabalho em equipe e o controle de áreas-chave.</p> <p>Aqui estão os principais pontos:</p> <p><strong>Bombsite A</strong>: Localizado em uma área mais ampla, cercado por paredes de pedra e com múltiplos pontos de entrada, como "main" e "caverna". A comunicação entre os jogadores e o uso de granadas são fundamentais para dominar este ponto.</p> <p><strong>Bombsite B</strong>: Um pouco mais fechado, com entradas por meio de "rampa" e "caverna". É comum ver confrontos rápidos nessa área, exigindo precisão nas execuções e controle da movimentação dos jogadores.</p> <p><strong>Meio</strong>: Como em muitos outros mapas, o controle do meio é crucial. Em Ancient, ele proporciona acesso rápido a ambos os bombsites e permite flanquear o inimigo de forma mais eficiente. O domínio desta área pode decidir o rumo da partida.</p> <p>O mapa é conhecido por suas passagens estreitas, que favorecem combates táticos e o uso inteligente de utilitários (smokes, flashes e molotovs). Ele oferece um grande desafio estratégico para jogadores, com muitos cantos e corredores estreitos, além de uma forte ênfase no domínio de áreas-chave.</p></p>'
+    document.getElementById('conteudobarra').innerHTML = '<img src="mapas/ancient.jpg"> <h1>Ancient</h1> <p><p><strong>Ancient</strong> é um mapa de <strong>Counter-Strike: Global Offensive</strong> que se passa em um cenário de ruínas antigas, inspirado por civilizações mesoamericanas, com vegetação densa e estruturas de pedra. Lançado em 2021, <strong>Ancient</strong> rapidamente se tornou um dos mapas favoritos por sua estética única e design desafiador, que valoriza o trabalho em equipe e o controle de áreas-chave.</p>  <p><strong>Bombsite A</strong>: Localizado em uma área mais ampla, cercado por paredes de pedra e com múltiplos pontos de entrada, como "main" e "caverna". A comunicação entre os jogadores e o uso de granadas são fundamentais para dominar este ponto.</p> <p><strong>Bombsite B</strong>: Um pouco mais fechado, com entradas por meio de "rampa" e "caverna". É comum ver confrontos rápidos nessa área, exigindo precisão nas execuções e controle da movimentação dos jogadores.</p><p>O mapa é conhecido por suas passagens estreitas, que favorecem combates táticos e o uso inteligente de utilitários. Ele oferece um grande desafio estratégico para jogadores, com muitos cantos e corredores estreitos, além de uma forte ênfase no domínio de áreas-chave.</p></p>'
+
+}
+
+function anubis(){
+
+    document.getElementById('barralateral').style.width = '600px'
+
+    document.getElementById('main').style.marginRight = '500px'
+
+    
+
+    document.getElementById('conteudobarra').innerHTML = '<img src="mapas/anubis.jpg"> <h1>Anubis</h1> <p><p><strong>Anubis</strong> é um mapa de <strong>Counter-Strike: Global Offensive (CS:GO)</strong> ambientado em uma temática egípcia, com estruturas inspiradas em templos antigos e ao redor de canais de água. A arquitetura e o design do mapa se destacam pela estética única, repleta de detalhes históricos, criando um ambiente imersivo e diferente dos outros mapas tradicionais do jogo.</p><p><strong>Bombsite A</strong>: Fica próximo de um templo e tem entradas múltiplas, incluindo uma área elevada e uma entrada pelo lado do "mid". O bombsite é aberto, o que favorece confrontos diretos e rápidas execuções, sendo essencial o uso de granadas para defender ou atacar.</p><p><strong>Bombsite B</strong>: Localizado em uma área mais fechada e próxima a um canal de água. Ele possui entradas por meio de túneis e caminhos estreitos, exigindo muita coordenação e precisão nas jogadas, tanto para defender quanto para executar o ataque.</p><p>O mapa é conhecido por seu design único, que favorece tanto jogadas agressivas quanto defesas organizadas. O uso de utilitários e o posicionamento correto são fundamentais para obter vantagem em Anubis, tornando-o um mapa dinâmico e cheio de possibilidades estratégicas.</p></p>'
+
+}
+
+function dust2(){
+
+    document.getElementById('barralateral').style.width = '600px'
+
+    document.getElementById('main').style.marginRight = '500px'
+
+    
+
+    document.getElementById('conteudobarra').innerHTML = '<img src="mapas/dust2.jpg"> <h1>Dust2</h1> <p><p><strong>Dust 2</strong> é um dos mapas mais icônicos de <strong>Counter-Strike: Global Offensive (CS:GO)</strong> e de toda a franquia Counter-Strike. Com uma ambientação em um cenário desértico do Oriente Médio, Dust 2 é amplamente reconhecido por seu design simples e equilibrado, sendo extremamente popular tanto em partidas casuais quanto em competições profissionais.</p><p><strong>Bombsite A</strong>: Localizado em uma área mais aberta, com entradas pela "catwalk" (subida pela curta) e pela "longa" (rampa longa). Esse bombsite favorece confrontos de longo alcance, sendo comum o uso de snipers (AWP) e granadas de fumaça para isolar áreas críticas.</p><p><strong>Bombsite B</strong>: Um local mais fechado, acessado por "túnel" e "meio", que requer rápidas execuções e boas defesas com o uso de utilitários. O bombsite B é conhecido por suas entradas apertadas, o que favorece jogadas defensivas bem organizadas e rápidas rotações.</p><p>Dust 2 é famoso por seu equilíbrio entre os lados Terrorista e Contra-Terrorista, favorecendo estratégias simples e confrontos diretos. Seu design clássico e fluidez fazem dele um dos mapas mais jogados e amados na história do CS:GO.</p></p>'
+
+}
+
+function inferno(){
+
+    document.getElementById('barralateral').style.width = '600px'
+
+    document.getElementById('main').style.marginRight = '500px'
+
+    
+
+    document.getElementById('conteudobarra').innerHTML = '<img src="mapas/inferno.jpg"> <h1>Inferno</h1> <p><p><strong>Inferno</strong> é um mapa clássico de <strong>Counter-Strike: Global Offensive (CS:GO)</strong>, conhecido por sua ambientação em uma vila italiana, com ruas estreitas, vielas e áreas urbanas históricas. Ele é amplamente utilizado tanto em partidas casuais quanto competitivas devido à sua complexidade estratégica e equilíbrio entre os lados Terrorista e Contra-Terrorista.</p><p><strong>Bombsite A</strong>: Fica em uma praça aberta, com várias entradas, incluindo o "meio", "tapete" e "varanda". O bombsite A é conhecido por oferecer boas oportunidades de defesa em múltiplos pontos, exigindo coordenação e uso inteligente de granadas para controlar as aproximações dos adversários.</p><p><strong>Bombsite B</strong>: Localizado no final de uma longa rua conhecida como "banana", é uma área mais fechada, com entradas limitadas, o que torna a defesa mais fácil se bem organizada. A "banana" é uma rota crucial para os ataques ao Bombsite B, sendo uma área de constante disputa.</p><p>Inferno é um mapa caracterizado por combates intensos em espaços fechados e rotas complexas, que exigem bom posicionamento e coordenação em equipe. Seu design favorece tanto jogadas agressivas quanto defensivas, sendo um dos mapas mais equilibrados e jogados no cenário competitivo de CS:GO.</p></p>'
+
+}
+
+function nuke(){
+
+    document.getElementById('barralateral').style.width = '600px'
+
+    document.getElementById('main').style.marginRight = '500px'
+
+    
+
+    document.getElementById('conteudobarra').innerHTML = '<img src="mapas/nuke.jpg"> <h1>Nuke</h1> <p><p><strong>Nuke</strong> é um mapa icônico de <strong>Counter-Strike: Global Offensive (CS:GO)</strong> com uma ambientação em uma instalação nuclear. Seu design vertical e multi-camadas o torna único, exigindo uma boa coordenação entre as equipes, já que as rotas e bombsites estão distribuídos em diferentes andares e níveis, tornando-o um mapa desafiador e tático.</p><p><strong>Bombsite A</strong>: Localizado no andar superior, é um espaço aberto com várias entradas, incluindo o "lobby", "porta", "ventilação" e "heaven" (plataforma elevada). O bombsite A requer precisão nas execuções e uma boa defesa com utilitários para impedir o avanço do time inimigo.</p><p><strong>Bombsite B</strong>: Situado no andar inferior, ele pode ser acessado por meio de "ventilação", "rampa" ou "secret". O bombsite B é mais isolado e de difícil acesso, o que favorece defesas sólidas, mas também oferece boas oportunidades para ataques rápidos e bem coordenados.</p><p><strong>Nuke</strong> é um mapa que favorece a defesa, com muitas oportunidades de surpreender o inimigo, e ao mesmo tempo oferece aos atacantes diversas formas de aproximação, se as rotas e utilitários forem bem utilizados. É um dos mapas mais estratégicos do CS:GO, exigindo conhecimento profundo das rotas e timings.</p></p>'
+
+}
+
+function overpass(){
+
+    document.getElementById('barralateral').style.width = '600px'
+
+    document.getElementById('main').style.marginRight = '500px'
+
+    
+
+    document.getElementById('conteudobarra').innerHTML = '<img src="mapas/overpass.jpg"> <h1>Overpass</h1> <p><p><strong>Overpass</strong> é um mapa de <strong>Counter-Strike: Global Offensive (CS:GO)</strong> ambientado em uma área urbana na Europa, com um design que mistura espaços abertos e corredores estreitos, como túneis e passagens subterrâneas. Ele é conhecido por favorecer jogadas estratégicas com utilitários e movimentação tática, sendo muito popular no cenário competitivo.</p><p><strong>Bombsite A</strong>: Localizado em uma praça próxima a um monumento, com entradas múltiplas pelo "banheiro" e "longa". O bombsite A é mais aberto, o que favorece lutas de longo alcance e o uso de snipers (AWP). É essencial o uso de granadas para cortar as linhas de visão e facilitar o avanço ou a defesa.</p><p><strong>Bombsite B</strong>: Posicionado próximo de uma construção de esgoto, é uma área mais fechada, com entradas pelo "túnel", "água" e "rampa". O bombsite B é conhecido por execuções rápidas e fortes confrontos em espaços curtos, exigindo boa coordenação e controle das entradas.</p><p>Com seu design que mistura áreas abertas e fechadas, Overpass é um mapa equilibrado que favorece tanto ataques rápidos quanto defesas bem organizadas. O uso inteligente de utilitários, controle de áreas chave e coordenação de equipe são essenciais para vencer nesse mapa.</p></p>'
 
 }
 
